7e63af4d512c906e16fbb5cec455399559a433cc
Fallen Angel <the.fallen.angel.9x@gmail.com>
2018-08-07 09:15:51 +0700

    download an image successfully

--- App.js
+++ App.js
@@ -1,27 +1,61 @@
 import React, { Component } from 'react';
 import {
-  Platform, StyleSheet, Text, View, TouchableOpacity
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+  Image,
+  PermissionsAndroid
 } from 'react-native';
 
-import firebase from "react-native-firebase";
 
+import firebase from "react-native-firebase";
+import RNFetchBlob from 'react-native-fetch-blob';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.Crashlytics = firebase.crashlytics();
     this.state = {
-      Demo: {
-        Name: "Ngo Kim Huynh",
-        Action: undefined,
-      },
+      URL: "https://img.onvshen.com:85/gallery/21501/23209/s/018.jpg",
+      ImageSource: " "
     };
   }
 
-  makeError = () => {
+  async makeError() {
     try {
-      console.log(this.state.Demo.Action.Hello);
-    } catch (error) {
-      console.log(firebase.crashlytics()._app.crash());
+      const tmp = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, {
+        "title": "Permission request",
+        "message": "Let me use your storage"
+      });
+      if (tmp == PermissionsAndroid.RESULTS.GRANTED) {
+        const dirs = RNFetchBlob.fs.dirs;
+        const path = dirs.DCIMDir + "/001.jpg";
+        RNFetchBlob
+          .config({
+            fileCache: true,
+          })
+          .fetch("GET", this.state.URL, {})
+          .then((result) => {
+            result.base64().then((data) => {
+              RNFetchBlob.fs.writeFile(path, data, "base64");
+              console.log(dirs);
+            })
+
+            this.setState({
+              ImageSource: "file://" + result.path()
+            })
+          })
+          .catch((error, code) => {
+            this.Crashlytics.log(error);
+          })
+      }
+    }
+    catch (error) {
+      console.log(error);
+      this.Crashlytics.log(error.toString());
     }
   }
 
@@ -29,10 +63,20 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <TouchableOpacity
+          style={{ backgroundColor: "#ff9999" }}
           onPress={this.makeError.bind(this)}
         >
-          <Text>{this.state.Demo.Name}</Text>
+
+          <Text style={{ padding: 20 }}>
+            Fallen Angel
+          </Text>
         </TouchableOpacity>
+        <Image
+          style={{ width: 150, height: 150, marginTop: 10, backgroundColor: "red", display: "none" }}
+          source={{
+            uri: this.state.ImageSource
+          }}
+        />
       </View>
     );
   }
--- android/app/src/main/java/com/downloadimagetool/MainApplication.java
+++ android/app/src/main/java/com/downloadimagetool/MainApplication.java
@@ -3,7 +3,10 @@ package com.downloadimagetool;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+
 import io.invertase.firebase.RNFirebasePackage;
+
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
                     new RNFirebasePackage(),
+                    new RNFetchBlobPackage(),
                     new RNFirebaseCrashlyticsPackage()
             );
         }
